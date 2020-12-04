655ea4cffdaee9b5bfae107904b3d3632d76b9d1
rez <benjaminresnicoff@icloud.com>
2020-12-04 14:13:48 -0600

    worked on server.js

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-// const express = require('express');
-// const connectDB = require('./config/db');
-// const path = require('path');
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-// const app = express();
-
-
-
-
-// // connectDB();
-
-// // Init Middleware
-// app.use(express.json());
-
-// // Define Routes
-// app.use('/api/users', require('./routes/api/users'));
-// app.use('/api/auth', require('./routes/api/auth'));
-// app.use('/api/profile', require('./routes/api/profile'));
-// app.use('/api/posts', require('./routes/api/posts'));
+const app = express();
 
 
 
 
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 
-// const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 
 
