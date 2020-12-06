ed53f7229a00db36b00db69d81c863ad11ae1580
Sam Fullerton <Samdfullerton@gmail.com>
2020-12-06 13:41:54 -0600

    test

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require ("express");
 const cors = require ("cors");
 const path = require ("path");
+const { mongo, Mongoose } = require("mongoose");
 
 const app = express();
 app.use(cors());
 
 require("./startup/db")();
 
+const CONNECTION_URL = "mongodb+srv://join:join123@cluster0.jmavo.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
 const port = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT< () => console.log('Server running on port: ${PORT}')))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
 
 app.listen(port, () => console.log(`Listenining on port ${port}`));
