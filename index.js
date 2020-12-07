856334f93143b0c3745585373bd43d397c9b1276
Steve Kavuu <mrkavuu@gmail.com>
2020-12-07 15:22:35 -0600

    deploy

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ app.use(express.json());
 
 // middleware
 app.use(express.json({extended:false}))
-app.get('/' , (req,res)=> res.send ('API is running'))
+app.use(express.static('client/build'));
+// app.get('/' , (req,res)=> res.send ('API is running'))
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
@@ -39,14 +40,14 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+// }
 
 const PORT = process.env.PORT || 5000;
 
